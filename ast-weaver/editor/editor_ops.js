5bd9a687eb9cff83064a46d9ab8223129412c576
Daniel Aston <astondani@wg401-a-08-10.mddn.vuw.ac.nz>
2019-05-14 15:31:39 +1200

    time to enumerate the types

---
+++ ast-weaver/editor/editor_ops.js
@@ -0,0 +1,21 @@
+
+function toSnippet(cutting){
+        let dummy=Babel.transform("",ast:true});
+	dummy.ast.body.push(epressionNode);
+	return Babel.transform(dummy.ast);
+}
+
+//store-ification
+function storeify(expressionNode,name){
+	let t=Babel.types;
+	declNode=t.variableDeclaration("let",[t.variableDeclarator(name,expressionNode)]);
+	return declNode;
+}
+//funcification
+function
+function funcify(expression,name){
+	let t=Babel.types;
+        declNode=t.functionDeclaration(name,[],t.blockStatement([
+	t.returnStatement(expressionNode)],[]);
+	return declNode;
+}
